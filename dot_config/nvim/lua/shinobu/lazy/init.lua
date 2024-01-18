2e2e93e76bcca0d8b301ad4e3bf72d04c6a0c3dc
Nazenano <113120126+Nazenano@users.noreply.github.com>
2024-01-18 09:54:36 +0100

    personal dotfiles

---
+++ dot_config/nvim/lua/shinobu/lazy/init.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "christoomey/vim-tmux-navigator",
+
+        lazy = false,
+    },
+
+    "williamboman/mason.nvim",
+    "github/copilot.vim",
+    "nvim-tree/nvim-web-devicons",
+    "tpope/vim-commentary",
+}
